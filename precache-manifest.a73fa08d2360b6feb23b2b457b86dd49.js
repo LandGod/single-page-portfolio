62145f7fc533e060e4c2d303b06b7b35e5525604
Dan Gold <tentativechaos@gmail.com>
2020-01-09 19:05:49 -0800

    Updates

---
+++ precache-manifest.a73fa08d2360b6feb23b2b457b86dd49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5676ee7a1e65f5f2c309667df92106b",
+    "url": "/single-page-portfolio/index.html"
+  },
+  {
+    "revision": "7115aa3da2f5f35dc3a2",
+    "url": "/single-page-portfolio/static/css/main.8b5a42e1.chunk.css"
+  },
+  {
+    "revision": "26d9582d65cce49d95c1",
+    "url": "/single-page-portfolio/static/js/2.6bdf107f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/single-page-portfolio/static/js/2.6bdf107f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7115aa3da2f5f35dc3a2",
+    "url": "/single-page-portfolio/static/js/main.5efc6338.chunk.js"
+  },
+  {
+    "revision": "8eaec837acac7b5bdbfc",
+    "url": "/single-page-portfolio/static/js/runtime-main.9e709307.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/single-page-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
