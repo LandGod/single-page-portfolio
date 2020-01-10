0c6e1c9d01f3d48a19a804848b51e772f0ce86b8
Dan Gold <tentativechaos@gmail.com>
2020-01-09 17:48:56 -0800

    Finished with about component styling (for now). Looks good on mobile and desktop. Media query tweaked. Random color change, totally not final.

--- src/components/About.js
+++ src/components/About.js
@@ -5,22 +5,22 @@ import TextBaloon from "./TextBaloon";
 class About extends React.Component {
   render() {
     return (
-      <div className="container mainContentArea">
+      <div className="container">
         <div className="row justify-content-end">
-
-        <div className="textBaloonComponent col-12 col-md-8 mt-5">
-                <TextBaloon />
-            </div>
-          <div className="col-12 col-md-4">
+          <div className="col-12 col-md-4 mainContentArea">
             <figure className="profilePic">
               <img
                 src={process.env.PUBLIC_URL + "/danielgoldphoto.jpg"}
                 className="img-fluid my-3"
               />
             </figure>
-            <div className="spacer"> </div>
-            <div className="iconBarComponent">
-              <IconBar />
+            <div className="overlapContainer">
+              <div className="textBaloonComponent mt-md-5">
+                <TextBaloon />
+              </div>
+              <div className="iconBarComponent">
+                <IconBar />
+              </div>
             </div>
           </div>
         </div>
