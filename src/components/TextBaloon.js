99d15b6b9fe00fb345555a916eedb836bd7d097e
Dan Gold <tentativechaos@gmail.com>
2020-01-09 16:33:44 -0800

    Laid out basic jsx structure of TextBaloon component.

---
+++ src/components/TextBaloon.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class TextBaloon extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h5>Summary</h5>
+            <p>
+              Web Developer with a background in the arts and theatre
+              technology. Educated at the University of Washington Coding
+              Bootcamp with skills in HTML, CSS, JavaScript, and Python.
+              Detail-oriented problem solver with a passion for iterating new
+              features to create better user experiences. Strengths in
+              creativity, teamwork, and quick learning. Equally at home with
+              front-end and back-end projects, and works well with others.
+            </p>
+            <h5>Skills</h5>
+            <p>
+              React, JavaScript, Node.js, Python, HTML, CSS, Express,
+              Handlebars, mySQL, MongoDB, REST APIs
+            </p>
+            <h5>Additional Skills</h5>
+            <p>Management, MS Office</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TextBaloon;
