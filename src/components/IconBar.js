c7cef55fe679437aedf7b9579bb51192ee02adb9
Dan Gold <tentativechaos@gmail.com>
2020-01-09 16:46:28 -0800

    Refactored the css for the IconBar component such that it is now modularized and seperate from other css.

--- src/components/IconBar.js
+++ src/components/IconBar.js
@@ -1,24 +1,25 @@
 import React from "react";
+import styles from "./styles/IconBar.module.css";
 
 class IconBar extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div className="container" style={styles}>
         <div className="row">
           <div className="col-12">
             <div className="row">
-              <div className="iconBarIcons">
-                <span className="col-2">
+              <div className={styles.iconBarIcons}>
+                <span className={`${styles.iconSpan} col-2`}>
                   <i class="fas fa-laptop"></i>
                 </span>
-                <span className="col-2">
+                <span className={`${styles.iconSpan} col-2`}>
                   <i class="fas fa-tablet-alt"></i>
                 </span>
-                <span className="col-2">
+                <span className={`${styles.iconSpan} col-2`}>
                   <i class="fas fa-mobile"></i>
                 </span>
               </div>
-              <div className="col-12 iconBarBar"></div>
+              <div className={`${styles.iconBarBar} col-12`}></div>
             </div>
           </div>
         </div>
