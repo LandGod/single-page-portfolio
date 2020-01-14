e5a5f522d5087d51c34c5abc52319037056c7793
Dan Gold <tentativechaos@gmail.com>
2020-01-14 12:32:12 -0800

    Laid out code for rendering project components within portfolio.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import ProjectCard from "./ProjectCard";
+import projects from '../assets/projects.json';
 
 class Portfolio extends React.Component {
+
   render() {
     return (
       <div className="container">
         <div className="row">
-          
+          {projects.map((project) => {
+            return (
+              <ProjectCard 
+                width={project.width}
+                imageName={project.imageName}
+                title={project.title}
+                repoLink={project.repoLink}
+                deployLink={project.deployLink}
+              />
+            )
+          })}
         </div>
     </div>
     );
