58ff8a641731c2f549b045566eeee6bd647c6ef4
Dan Gold <tentativechaos@gmail.com>
2020-01-19 00:06:19 -0800

    Added extra media queries to clean up header positions at various screen sizes.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,7 +8,7 @@ class Portfolio extends React.Component {
     return (
       <div className="container">
         <div className="row">
-          <div className='col-12 text-center'>
+          <div className='col-12 text-center portfolioHOne'>
             <h1>Portfolio</h1>
           </div>
         </div>
