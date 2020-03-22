81b85477b8d5a25d641adf502a451a211761c339
Dan Gold <tentativechaos@gmail.com>
2020-03-22 02:11:48 -0700

    Implemented css transtions for pull-down reveal.

--- src/components/ButtonDrawer.js
+++ src/components/ButtonDrawer.js
@@ -9,20 +9,23 @@ function ButtonDrawer(props) {
   };
 
   return (
-    <div className={styles.containingDiv}>
-      <hr className={styles.hr} />
-      <div className={styles.box}>Filter</div>
-      <button className={`${styles.plusButton} btn btn-link `}>
-        <i className={` ${styles.plusIcon} fas fa-plus-circle`}></i>
-        <div className={styles.iBackground} ></div>
-      </button>
-      <div
-        className={`${styles.buttonContainer} ${styles.hidden} container`}
-        onClick={toggleDawer}
-      >
-        {props.children}
+    <>
+      <div className={styles.containingDiv}>
+        <hr className={styles.hr} />
+        <div className={styles.box}>Filter</div>
+        <button
+          className={`${styles.plusButton} btn btn-link `}
+          onClick={toggleDawer}
+        >
+          <i className={` ${styles.plusIcon} fas fa-plus-circle`}></i>
+          <div className={styles.iBackground}></div>
+        </button>
+        <div className={styles.spacer} style={{height:`${open ? "5vh" : "15vh"}`}}> </div>
       </div>
-    </div>
+        <div className={`${styles.buttonContainer} container`} style={{height:`${open ? "18vh" : "0"}`}}>
+          {props.children}
+        </div>
+    </>
   );
 }
 
