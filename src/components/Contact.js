1b51edef97f2bb4f84a274d3cf192143d3479931
Dan Gold <tentativechaos@gmail.com>
2020-04-09 17:53:07 -0700

    Refactored  contact and contact cards such that colums are handled outside of contactCard. Added tooltip to component around email.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,36 +1,56 @@
 import React from "react";
+import ToolTip from "./ToolTip";
 import ContactCard from "./ContactCard";
 
 class Contact extends React.Component {
+  state = { showEmailPopup: false };
+
+  togglePopup = () => {
+    this.setState({ showEmailPopup: !this.state.showEmailPopup });
+  };
+
   render() {
     return (
       <div className="container" id="contactSection">
         <div className="row">
           <div className="col-12 text-center">
-            <h1 className="contactHOne" >Contact</h1>
+            <h1 className="contactHOne">Contact</h1>
           </div>
         </div>
         <div className="row ">
-        <ContactCard
-            imageName="twitter.svg"
-            title="Daniel Gold on Twitter"
-            link="https://twitter.com/DanGoldLandGod"
-          />
-          <ContactCard
-            imageName="linkedin.svg"
-            title="Daniel Gold on LinkedIn"
-            link="https://www.linkedin.com/in/danjasongold/"
-          />
-          <ContactCard
-            imageName="github.svg"
-            title="LandGod on GitHub"
-            link="https://www.github.com/landgod"
-          />
-          <ContactCard
-            imageName="email.svg"
-            title="DanielJasonGold@gmail.com"
-            customClickAction={()=>console.log("Log email (test)")}
-          />
+          <div className="col-6 col-md-3 p-1 p-md-2">
+            <ContactCard
+              imageName="twitter.svg"
+              title="Daniel Gold on Twitter"
+              link="https://twitter.com/DanGoldLandGod"
+            />
+          </div>
+          <div className="col-6 col-md-3 p-1 p-md-2">
+            <ContactCard
+              imageName="linkedin.svg"
+              title="Daniel Gold on LinkedIn"
+              link="https://www.linkedin.com/in/danjasongold/"
+            />
+          </div>
+          <div className="col-6 col-md-3 p-1 p-md-2">
+            <ContactCard
+              imageName="github.svg"
+              title="LandGod on GitHub"
+              link="https://www.github.com/landgod"
+            />
+          </div>
+          <div className="col-6 col-md-3 p-1 p-md-2">
+            <ToolTip
+              content={<div>DanielJasonGold@gmail.com</div>}
+              showTooltip={this.state.showEmailPopup}
+            >
+              <ContactCard
+                imageName="email.svg"
+                title="DanielJasonGold@gmail.com"
+                customClickAction={this.togglePopup}
+              />
+            </ToolTip>
+          </div>
         </div>
       </div>
     );
