919ef23c833b73ae875851a5100b478f5b3e383c
Dan Gold <tentativechaos@gmail.com>
2020-04-14 16:33:15 -0700

    Refactored media query in About.js to pure css.

--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,10 @@ import React from "react";
 class About extends React.Component {
   render() {
     return (
-      <div className={`mainContentArea container`} id="aboutSection">
+      <div className="mainContentArea container" id="aboutSection">
         <div className="row justify-content-around">
           {/* Column containing text (with media query) */}
-          <div
-            className={`${
-              this.props.mobileSize
-                ? 'aboutTextContainerMobile'
-                : 'aboutTextContainer'
-            } col-12 col-md-8`}
-          >
+          <div className="aboutTextContainerMobile">
             {/* Header and paragraph tag must be enclosed or flexbox will put them side-by-side when centering */}
             <div>
               <div className="introHeader">Hi, I'm Dan!</div>
@@ -24,11 +18,13 @@ class About extends React.Component {
           </div>
 
           {/* Column containing profile picture */}
-          <div className={`profilePicContainer col-12 col-md-4`}>
+          <div className="profilePicContainer col-12 col-md-4">
             <figure>
               <img
-                src={process.env.PUBLIC_URL + "/misc_images/danielgoldphoto.jpg"}
-                className={`profilePic img-fluid my-3`}
+                src={
+                  process.env.PUBLIC_URL + "/misc_images/danielgoldphoto.jpg"
+                }
+                className="profilePic img-fluid my-3"
                 alt="Daniel Gold"
               />
             </figure>
