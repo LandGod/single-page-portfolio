76f3e774b20c64bd83d21d0e5e28c6d999b9b08f
Dan Gold <tentativechaos@gmail.com>
2020-04-14 16:25:08 -0700

    Moved all styling for About.js to index.css and removed style import and reference in class names.

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,22 @@
 import React from "react";
-import styles from "./styles/About.module.css";
 
 class About extends React.Component {
   render() {
     return (
-      <div className={` ${styles.mainContentArea} container`} id="aboutSection">
+      <div className={`mainContentArea container`} id="aboutSection">
         <div className="row justify-content-around">
           {/* Column containing text (with media query) */}
           <div
             className={`${
               this.props.mobileSize
-                ? styles.aboutTextContainerMobile
-                : styles.aboutTextContainer
+                ? 'aboutTextContainerMobile'
+                : 'aboutTextContainer'
             } col-12 col-md-8`}
           >
             {/* Header and paragraph tag must be enclosed or flexbox will put them side-by-side when centering */}
             <div>
-              <div className={styles.introHeader}>Hi, I'm Dan!</div>
-              <p className={styles.introText}>
+              <div className="introHeader">Hi, I'm Dan!</div>
+              <p className="introText">
                 Web Developer with a background in the arts and theatre
                 technology
               </p>
@@ -25,11 +24,11 @@ class About extends React.Component {
           </div>
 
           {/* Column containing profile picture */}
-          <div className={` ${styles.profilePicContainer} col-12 col-md-4`}>
+          <div className={`profilePicContainer col-12 col-md-4`}>
             <figure>
               <img
                 src={process.env.PUBLIC_URL + "/misc_images/danielgoldphoto.jpg"}
-                className={`${styles.profilePic} img-fluid my-3`}
+                className={`profilePic img-fluid my-3`}
                 alt="Daniel Gold"
               />
             </figure>
