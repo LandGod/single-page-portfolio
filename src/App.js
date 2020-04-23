b9960f77d07a3de92e7f393c26cf01b1ef35e035
Dan Gold <tentativechaos@gmail.com>
2020-04-22 19:27:03 -0700

    Moved all css out of index.css into mainApp.css. Importing boostrap.css and mainApp.css in App.js so that webpack can bundle and minify everything.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Contact from "./components/Contact";
 import NavBar from "./components/NavBar";
 import MediaContextProvider from "./contexts/MediaContext";
 
+// Import css. Important that mainApp is imported AFTER boostrap so that it can override certain boostrap styles.
+// Note that boostrap.css is a cherry-picked version of the normal boostrap css file leaving out most of the bit that I'm not using.
+import "./bootstrap.css";
+import "./mainApp.css";
+
 function App() {
   return (
     <div className="App">
