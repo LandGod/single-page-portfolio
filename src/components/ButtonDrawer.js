bf8c05a1a1b803f604423b12cb102e17ebcd1f28
Dan Gold <tentativechaos@gmail.com>
2020-04-22 20:09:40 -0700

    Replaced font-awesome symbol with inline svg from shape.so

--- src/components/ButtonDrawer.js
+++ src/components/ButtonDrawer.js
@@ -51,16 +51,29 @@ function ButtonDrawer(props) {
           aria-expanded={props.open}
         >
           <div className="plus__label">Filter</div>
-          <div className="plus__inner btn btn-link">
-            <i
-              className="plus__icon fas fa-plus-circle"
-              style={{
-                transform: `${
-                  props.open ? "rotate(45deg) translateX(1.9%)" : "rotate(0deg)"
-                }`,
-              }}
-            ></i>
-            <div className="plus__background"></div>
+          <div className="plus__inner">
+            <svg className="plus__icon" width="24" height="24" style={ props.open ? {transform:"rotate(45deg)"} : null}>
+              <rect width="24" height="24" fill="none" rx="0" ry="0" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M11 17C11 17.55 11.45 18 12 18C12.55 18 13 17.55 13 17V13H17C17.55 13 18 12.55 18 12C18 11.45 17.55 11 17 11H13V7C13 6.45 12.55 6 12 6C11.45 6 11 6.45 11 7V11H7C6.45 11 6 11.45 6 12C6 12.55 6.45 13 7 13H11V17Z"
+                fill="#ffffff"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM12 18C11.45 18 11 17.55 11 17V13H7C6.45 13 6 12.55 6 12C6 11.45 6.45 11 7 11H11V7C11 6.45 11.45 6 12 6C12.55 6 13 6.45 13 7V11H17C17.55 11 18 11.45 18 12C18 12.55 17.55 13 17 13H13V17C13 17.55 12.55 18 12 18Z"
+                fill="#000000"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM12 18C11.45 18 11 17.55 11 17V13H7C6.45 13 6 12.55 6 12C6 11.45 6.45 11 7 11H11V7C11 6.45 11.45 6 12 6C12.55 6 13 6.45 13 7V11H17C17.55 11 18 11.45 18 12C18 12.55 17.55 13 17 13H13V17C13 17.55 12.55 18 12 18Z"
+                fill="white"
+                fill-opacity="0.01"
+              />
+            </svg>
           </div>
         </button>
         <span className="sr-only" role="alert">
